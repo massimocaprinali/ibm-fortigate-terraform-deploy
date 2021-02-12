788cf2ba3d5f46b599fe061ec6ba0a36279b84f0
Joel Cripps <jcripps@fortinet.com>
2021-02-12 08:49:51 -0800

    rename image in lowercase

--- image.tf
+++ image.tf
@@ -4,7 +4,7 @@ resource "random_uuid" "test" { }
 resource "ibm_is_image" "vnf_custom_image" {
  // depends_on       = ["random_uuid.test"]
   href             = var.image
-  name             = "${var.cluster_name}-fortinetCustomImage-${random_string.random_suffix.result}"
+  name             = "${var.cluster_name}-fortigate-custom-image-${random_string.random_suffix.result}"
   operating_system = "fortigate"
 //  resource_group = "${data.ibm_is_subnet.vnf_subnet.resource_group}"
 
