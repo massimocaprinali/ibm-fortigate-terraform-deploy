847ea16e1ff77f737367b2e8e2650b09e69e412d
Joel Cripps <jcripps@fortinet.com>
2021-03-18 10:06:40 -0700

    added descriptions changed some values for the catalog

--- vars.tf
+++ vars.tf
@@ -1,23 +1,34 @@
-# Your SSH key
+// Your SSH key
 variable "ssh_public_key" {
   default = ""
 }
-
+// Magic Value for Calalog Validation that initlizes terraform with a specific version.
+// Only needed in IBM catalog.
+variable "TF_VERSION" {
+ default = "0.13"
+ description = "Terraform version to be used in validation"
+}
 // IBM Regions
 variable "region" {
   type    = string
   default = "us-south"
+  description = "Deployment Region"
+
 }
 // IBM availability zones
 variable "zone1" {
   type    = string
   default = "us-south-1"
+  description = "Deployment Zone."
+
 }
 // Name will be in the format of cluster_name-RESOURCE-randomSuffix to be easily identifiable.
 // Name must be lowercase
 variable "cluster_name" {
   type    = string
   default = "fortigate-terraform"
+  description = "Cluster name will be appended by a random Suffix to prevent collisions and allow easier identification."
+
 }
 // Random Suffix to avoid name collisions and identify cluster.
 resource "random_string" "random_suffix" {
@@ -25,16 +36,6 @@ resource "random_string" "random_suffix" {
   special          = true
   override_special = ""
   min_lower        = 4
-}
-
-variable "primary_subnet" {
-  type    = string
-  default = "172.16.0.0/24"
-}
-
-variable "secondary_subnet" {
-  type    = string
-  default = "172.16.8.0/24"
 }
 
 // FortiOS Custom Image ID
@@ -51,16 +52,12 @@ variable "ibmcloud_api_key" {
 // See: https://cloud.ibm.com/docs/vpc?topic=vpc-profiles
 variable "profile" {
   default = "cx2-2x4"
+  description = "VM size and family"
 }
 
 // Bootstrap configuration file
 variable "user_data" {
   type    = string
   default = "user_data.conf"
-}
-
-// FortiGate License. When Providing via Schematics ensure this is a sensitive feild.
-variable "license" {
-  type    = string
-  default = ""
+  description = "The Custom Bootstrap Data file."
 }
