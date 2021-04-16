57cefda761c7acb4a9099a06f5e4e897d4103117
Joel Cripps <jcripps@fortinet.com>
2021-04-16 15:14:25 -0700

    changing to use subnet uuid instead of name

--- vpc.tf
+++ vpc.tf
@@ -2,10 +2,11 @@ data "ibm_is_vpc" "vpc1" {
   name = var.vpc
 }
 data "ibm_is_subnet" "subnet1" {
-  name = var.subnet1
+  identifier = var.subnet1
 }
+
 data "ibm_is_subnet" "subnet2" {
-  name = var.subnet2
+  identifier = var.subnet2
 }
 resource "ibm_is_vpc_routing_table" "fgt_route_table" {
   name = "${var.cluster_name}-port1-${random_string.random_suffix.result}"
