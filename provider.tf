e5857fc75546a31e267d4f532304e1ba504412e2
fshuva <fshuva@fortinet.com>
2022-04-18 15:14:51 -0700

    Updating minimum version requirements & tagged releases

--- provider.tf
+++ provider.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "> 0.13.0"
+  required_version = "> 1.0.0"
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "~> 1.36.0"
+      version = "~> 1.40.1"
     }
   }
 }
@@ -12,6 +12,6 @@ terraform {
 provider "ibm" {
   region           = var.region
   ibmcloud_api_key = var.ibmcloud_api_key
-  generation       = 2
+  # generation       = 2
 
 }
